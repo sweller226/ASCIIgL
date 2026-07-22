bd3507f450e87b4617ba50fd2d1f5a6034497037
Steven Weller <stevenweller091@gmail.com>
2026-07-21 22:54:50 -0400

    added water as a placeable block

--- ASCIICraft/src/game/Game.cpp
+++ ASCIICraft/src/game/Game.cpp
@@ -1041,7 +1041,7 @@ void Game::InitializeItemDefinitions() {
         );
     };
 
-    // === Block items (all registered block types except air and water) ===
+    // === Block items (all registered block types except air) ===
     itemRegistry.RegisterBlockItem(registry, "minecraft:dandelion",        "Dandelion", 64, ecs::components::ItemGuiMeshTransform::DefaultBlockThirdPerson(), ecs::components::ItemHeldMeshTransform::DefaultGeneratedFirstPerson());
     itemRegistry.RegisterBlockItem(registry, "minecraft:poppy",            "Poppy", 64, ecs::components::ItemGuiMeshTransform::DefaultBlockThirdPerson(), ecs::components::ItemHeldMeshTransform::DefaultGeneratedFirstPerson());
     itemRegistry.RegisterBlockItem(registry, "minecraft:tall_grass",       "Tall Grass", 64, ecs::components::ItemGuiMeshTransform::DefaultBlockThirdPerson(), ecs::components::ItemHeldMeshTransform::DefaultGeneratedFirstPerson());
@@ -1065,6 +1065,7 @@ void Game::InitializeItemDefinitions() {
     itemRegistry.RegisterBlockItem(registry, "minecraft:blue_wool",        "Blue Wool");
     itemRegistry.RegisterBlockItem(registry, "minecraft:green_wool",       "Green Wool");
     itemRegistry.RegisterBlockItem(registry, "minecraft:farmland",         "Farmland");
+    itemRegistry.RegisterBlockItem(registry, "minecraft:water",            "Water");
 
     // === Resources / Materials ===
     itemRegistry.RegisterResourceItem(registry, "minecraft:coal",       "Coal",       itemLayer("minecraft:items/coal"));
